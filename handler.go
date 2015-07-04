420adef1f6b9b50c3b97bae34c53815c5ec87e9c
Adam Ryman <adamryman@gmail.com>
2015-07-04 16:51:33 -0700

    Adding different httprouter

--- handler.go
+++ handler.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "Hi there, I loves %s!", r.URL.Path[1:])
 }
--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	//	"net/http"
 	"fmt"
 	"github.com/jinzhu/gorm"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
 	"os"
 )
 
 var mygorm, _ = gorm.Open("postgres", "user=postgres dbname=ambition password=ambition")
 
 var database = Database{mygorm}
+
 var _commands = map[string]func(){
 	"seed":   database.seedTables,
 	"clear":  database.clearTables,
@@ -20,12 +22,16 @@ var _commands = map[string]func(){
 func main() {
 	database.d.DB()
 	//need to add check that os.Args is greater than 1
-	command := _commands[os.Args[1]]
-	if command != nil {
-		command()
-	} else {
-		fmt.Println("Command not found")
+	if len(os.Args) > 2 {
+		command := _commands[os.Args[1]]
+		if command != nil {
+			command()
+		} else {
+			fmt.Println("Command not found")
+		}
 	}
-	//	http.HandleFunc("/", handler)
-	//	http.ListenAndServe(":8081", nil)
+	router := httprouter.New()
+	router.GET("/", handler)
+
+	http.ListenAndServe(":3000", router)
 }
