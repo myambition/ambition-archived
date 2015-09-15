ab204f26e335910ced8238e4aa41019982b61a0f
Adam Ryman <adamryman@gmail.com>
2015-09-15 11:25:01 -0700

    Attempt to add post request for clicking item

--- static/main.js
+++ static/main.js
@@ -1,3 +1,5 @@
+var serverRoot = "http://localhost:3000"
+
 $('.list-group-item').on({
         'mousedown' : function() { activate(this); },
         'touchstart' : function() { activate(this); },
@@ -8,6 +10,13 @@ $('.list-group-item').on({
 function activate(that) {
     $('#main').find('.active').removeClass('active');
     $(that).addClass('active');
+
+    $.ajax({
+        type: "POST",
+        url: serverRoot + "/actions/" + $(that).attr("dbid"),
+        data: { time: Date.now().toISOString() },
+        sucess: function () { alert("woo");}
+    })
 }
 
 function deactivate(that) {
